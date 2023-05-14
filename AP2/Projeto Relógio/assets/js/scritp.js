e1ab8003404fa4b1fcbcac139353fae2c9b52db9
Eduarda Moraes <99514703+eduardamoraesg@users.noreply.github.com>
2023-05-14 16:31:23 -0300

    Update scritp.js

--- AP2/Projeto Relógio/assets/js/scritp.js
+++ AP2/Projeto Relógio/assets/js/scritp.js
@@ -13,8 +13,9 @@ const relogio = setInterval(function time() {
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
     if (sec < 10) sec = '0' + sec;
-
+    
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = sec;
+    // text.Content atribui o valor das variáveis aos ids
 })
